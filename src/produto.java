fb423ecd555ca3241ecf5e98f26052a6c125f354
Luiz Felipe <felipeceltic@outlook.com>
2022-09-21 18:01:47 -0300

    Produto

---
+++ src/produto.java
@@ -0,0 +1,23 @@
+public class produto {
+
+  private String desc;
+  private int cod;
+
+
+  public String getDesc() {
+    return this.desc;
+  }
+
+  public void setDesc(String desc) {
+    this.desc = desc;
+  }
+
+  public int getCod() {
+    return this.cod;
+  }
+
+  public void setCod(int cod) {
+    this.cod = cod;
+  }
+
+}
